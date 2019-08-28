11434b2004615569ff5da2833ac8c6b316c10ed1
Germinal Camps <gcampsbarjau@gmail.com>
2019-08-28 17:53:58 +0200

    Simplified Reverse Linked List approach to solution, and guide

--- src/reverse_linked_list.js
+++ src/reverse_linked_list.js
@@ -1,15 +1,14 @@
 /*------------------
-Finish the "reverseLinkedList" method.
+Finish the "reverseList" method.
 This method should reverse the linkedList
 so that the head and tail switch, and all
 pointers move into the opposite direction.
 
 Hint: The code has already been started
 for you. DRAW THIS OUT and look at the test for
-the example. Use the insertAfter and removeFromTail
-methods in your solution.
+the example. You don't need to use the insertAfter
+method in your solution.
 ------------------*/
-
 
 class Node {
   constructor(value) {
@@ -44,31 +43,24 @@ class LinkedList {
     }
   }
 
-  removeFromTail() {
-    let currentNode = this.head;
-    while (currentNode.next !== this.tail) {
-      currentNode = currentNode.next;
-    }
-    let removedNode = this.tail;
-    this.tail = currentNode;
-    this.tail.next = null;
-    return removedNode;
-  }
-
   reverseList() {
-    // create a variable to store the reference to this.head
-    // remove the tail, and reassign that node to this.head
-    // connect this.head to the previous head that you stored
-
-    // create a variable (refNode) to keep track of the the location of where you will insert the next node (start by assigning it to this.head)
-
-    // while the previous head value does not equal the tail value
-      // remove the tail node
-      // insert it after the refNode
-      // reassign the refNode to the node you just added
-
+    // The idea is start with the head and go advancing in the list, changing the direction of pointers
+    // So the current head will be pointing "the other way" (which means null), and therefore will become the new tail of the list
+    // Then we will take the following node on the list, and reverse its pointer, so it points to what has previously the Head (but now is the tail)
+    // And we will do this progressively with each node, till we reach the "current tail", that will become the new head
+    // Your code starts here:
+    // Create a variable 'node' to store the reference to this.head. This var will keep track of the "current" node as we advance on the list
+    // Create another var 'initialHead', to also point to this.head. We will need it at the end.
+    // Create a variable to store the "previous" node. Since we start at the head, "previous" should be null when initialized (because there's nothing "before" the head)
+    // while there is a node to visit (so, node exists)
+    // create a var to save the location of the next node, or you will lose it!!!
+    // reverse the pointer, it should now point to the "previous" node
+    // increment previous to current node
+    // increment node to next node (you saved it) or null at end of list
+    // end of while
+    // make the head point to what was, until now, the tail
+    // make the tail point to what was initially the head
   }
-
 }
 
 module.exports = LinkedList;
