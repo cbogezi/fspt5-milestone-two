04836ca50eacdba25c26afe810cc1ba4641f955b
Germinal Camps <gcampsbarjau@gmail.com>
2020-01-16 22:13:06 +0100

    Clarifications on test instructions

--- src/classes/building.js
+++ src/classes/building.js
@@ -5,6 +5,8 @@ quality of tests you write for this!
 1. Create a class named Building. (already done for you)
 2. Add two properties (your choice) and test them.
 3. Add two methods (your choice) and test them.
+You should test that they exist and that they work correctly.
+You can add as many tests as you see fit.
 ------------------*/
 
 class Building {}
